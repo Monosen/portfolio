0ceb70e745323f8f17d8d5a15d12a55961506dd7
Monosen <edinson.bolanos00@usc.edu.co>
2022-11-29 17:38:01 -0500

    feat(astro(contact)): adding new contact page

---
+++ monosen/src/components/contact/Form.tsx
@@ -0,0 +1,34 @@
+import "./Form.style.scss";
+
+export const Form = () => {
+  return (
+    <form className="form-one text-align-center">
+      <div className="form">
+        <div className="form-background"></div>
+        <input type="text" required />
+        <label className="lbl-name">
+          <span className="text-name">Name</span>
+        </label>
+      </div>
+      <div className="form">
+        <input type="mail" required />
+        <label className="lbl-name">
+          <span className="text-name">Email</span>
+        </label>
+      </div>
+      <div className="form">
+        <input type="text" required />
+        <label className="lbl-name">
+          <span className="text-name">Subject</span>
+        </label>
+      </div>
+      <div className="form-message">
+        <textarea cols={30} rows={10} required></textarea>
+        <label className="lbl-name">
+          <span className="text-name-one">Your Message</span>
+        </label>
+      </div>
+      <input className="bottom" type="submit" value="Submit" />
+    </form>
+  );
+};
