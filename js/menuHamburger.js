c577a36d76b291cf703dc6d990c068bf45e27e3a
Monosen <edinson.bolanos00@usc.edu.co>
2022-12-09 01:53:03 -0500

    feat(astro): moving project to start

--- js/menuHamburger.js
+++
@@ -1,8 +0,0 @@
-/* animacion botom menu*/ 
-
-const nav = document.querySelector('#hamburger button');
-const navMenu = document.querySelector('.nav-list'); 
-nav.addEventListener('click', () => {
-    nav.classList.toggle('open');
-    navMenu.classList.toggle('nav-completo');
-})
--- js/preload.js
+++
@@ -1,8 +0,0 @@
-/* preload */
-
-window.onload = function () {
-    const preload = document.querySelector('#preload');
-    preload.style.visibility = 'hidden';
-    preload.style.opacity = '0';
-    preload.style.display = 'none';
-}
