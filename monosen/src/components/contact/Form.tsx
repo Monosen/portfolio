e26c47e53a6587a51f945d901973d8f300e29c6a
Monosen <edinson.bolanos00@usc.edu.co>
2022-12-08 21:59:46 -0500

    feat(astro(contact)): adding validation to contact form

--- monosen/src/components/contact/Form.tsx
+++ monosen/src/components/contact/Form.tsx
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form";
 import "./Form.style.scss";
 
 type Inputs = {
@@ -8,34 +9,103 @@ type Inputs = {
 };
 
 export const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSendMessage = async ({ name, email, message, subjet }: Inputs) => {
+    console.log(name, email, message, subjet);
+  };
+
+  const isValidEmail = (email: string): boolean => {
+    const match = String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+
+    return !!match;
+  };
+
+  const isEmail = (email: string): string | undefined => {
+    return isValidEmail(email) ? undefined : "El correo no parece ser válido";
+  };
+
   return (
-    <form className="form-one text-align-center">
+    <form
+      onSubmit={handleSubmit(onSendMessage)}
+      className="form-one text-align-center"
+      noValidate
+    >
+      {errors.name && <p className="text-error">{errors.name.message}</p>}
       <div className="form">
         <div className="form-background"></div>
-        <input type="text" required />
+        <input
+          {...register("name", {
+            required: "el nombre es requerido",
+            minLength: { value: 1, message: "minimo 1 caracter" },
+          })}
+          name="name"
+          type="text"
+          required
+        />
+
         <label className="lbl-name">
-          <span className="text-name">Name</span>
+          <p className="text-name">nombre</p>
         </label>
       </div>
+      {errors.email && <p className="text-error">{errors.email.message}</p>}
+
       <div className="form">
-        <input type="mail" required />
+        <input
+          {...register("email", {
+            required: "el correo es requerido",
+            validate: isEmail,
+          })}
+          name="email"
+          type="mail"
+          required
+        />
         <label className="lbl-name">
-          <span className="text-name">Email</span>
+          <p className="text-name">correo electronico</p>
         </label>
       </div>
+
+      {errors.subjet && <p className="text-error">{errors.subjet.message}</p>}
       <div className="form">
-        <input type="text" required />
+        <input
+          {...register("subjet", {
+            required: "el sujeto es requerido",
+            minLength: { value: 1, message: "minimo 1 caracter" },
+          })}
+          name="subjet"
+          type="text"
+          required
+        />
         <label className="lbl-name">
-          <span className="text-name">Subject</span>
+          <p className="text-name">sujeto</p>
         </label>
       </div>
+
+      {errors.message && <p className="text-error">{errors.message.message}</p>}
       <div className="form-message">
-        <textarea cols={30} rows={10} required></textarea>
+        <textarea
+          {...register("message", {
+            required: "el mensage es requerido",
+            minLength: { value: 10, message: "minimo 10 caracteres" },
+          })}
+          name="message"
+          cols={30}
+          rows={10}
+          required
+        ></textarea>
         <label className="lbl-name">
-          <span className="text-name-one">Your Message</span>
+          <p className="text-name-one">tu mensage</p>
         </label>
       </div>
-      <input className="bottom" type="submit" value="Submit" />
+      <input className="bottom" type="submit" value="Enviar" />
     </form>
   );
 };
